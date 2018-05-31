04eeeb67ce12196e418b859d3a4914c6b030cee4
Priyank Kashyap <kashyap.eeng@gmail.com>
2018-05-31 08:18:57 +0200

    SVM and Email Processing

--- Support Vector Machines/ex6/dataset3Params.m
+++ Support Vector Machines/ex6/dataset3Params.m
@@ -10,7 +10,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 % You need to return the following variables correctly.
 C = 1;
 sigma = 0.3;
-
+value= [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10];
+error_min = inf;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -22,9 +23,19 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
-
-
-
+for i=1:length(value)
+    for j=1:length(value)
+        model = svmTrain(X,y,value(i), @(x1, x2)gaussianKernel(x1,x2,value(j)));
+        prediction =svmPredict(model,Xval);
+        err = mean(double(prediction ~=yval));
+        if err < error_min
+            error_min=err;
+            C=value(i);
+            sigma=value(j);
+            fprintf('[C, sigma] = [%f %f]\n', C, sigma)
+        end
+    end
+end
 
 
 
